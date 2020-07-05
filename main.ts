5ecd317f8f5b6d2753bd998a4206379fcd563d78
Peter Heldens <peter.heldens@live.nl>
2020-07-05 10:32:39 +0200

    added loop 25

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
-basic.forever(function () {
-	
-})
+function plotAt (index: number) {
+    index |= 0
+    y = Math.floor(index / 5)
+    x = Math.floor(index % 5)
+    led.plot(x, y)
+}
+let x = 0
+let y = 0
+for (let i = 0; i <= 24; i++) {
+    plotAt(i)
+    pause(100)
+}
